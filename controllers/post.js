20959416227c849ed163d3be682bdbf37c604e30
Havold <21521479@gm.uit.edu.vn>
2024-09-22 18:11:42 +0700

    Create method add new Post.

--- controllers/post.js
+++ controllers/post.js
@@ -1,7 +1,9 @@
+import moment from "moment";
 import { db } from "../connect.js";
 import jwt from "jsonwebtoken";
 
 export const getPosts = (req, res) => {
+  // VERIRY LOGGED IN
   const token = req.cookies.accessToken;
 
   if (!token) return res.status(401).json("Not logged in!");
@@ -20,4 +22,28 @@ export const getPosts = (req, res) => {
       return res.status(200).json(data);
     });
   });
+};
+
+export const addPost = (req, res) => {
+  // VERIFY LOGGED IN
+  const token = req.cookies.accessToken;
+
+  if (!token) return res.status(401).json('Not logged in!');
+  jwt.verify(token, 'secretKey', (err, userInfo) => {
+    if (err) return res.status(403).json('Token is not valid!');
+
+    const q = 'INSERT INTO posts (`desc`, `img`, `userId`, `createdAt`) VALUES(?)'
+
+    const values = [
+        req.body.desc,
+        req.body.img,
+        userInfo.id,
+        moment(Date.now()).format('YYYY-MM-DD HH::mm:ss')
+    ]
+
+    db.query(q, [values], (err, data) => {
+        if (err) return res.status(500).json(err);
+        return res.status(200).json('New post has been created!')
+    })
+  })
 };

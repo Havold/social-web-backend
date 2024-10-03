35007531a8ea80576c208b6fa049b2b26c4c06db
Trương Hữu Thọ <96384986+Havold@users.noreply.github.com>
2024-10-03 15:53:55 +0700

    Create method PUT user.

--- controllers/user.js
+++ controllers/user.js
@@ -8,3 +8,32 @@ export const getUser = (req, res) => {
         return res.status(200).json(data[0])
     })
 }
+
+export const updateUser = (req, res) => {
+    // VERIFY LOGGED IN
+    const token = req.cookies.accessToken;
+    if (!token) return res.status(401).json('NOT LOGGED IN!');
+    
+    jwt.verify(token, 'secretKey', (err, userInfo) => {
+        if (err) return res.status(403).json('TOKEN IS NOT VALID!');
+        
+        const q = 'UPDATE users SET `name` = ? , `username` = ?, `email` = ?, `city`=?, `website`=?, `phone`=?, `coverPic`=?, `profilePic`=? WHERE id = ?';
+        const values = [
+            req.body.name,
+            req.body.username,
+            req.body.email,
+            req.body.city,
+            req.body.website,
+            req.body.phone,
+            req.body.coverPic,
+            req.body.profilePic,
+            userInfo.id,
+        ];
+
+        db.query(q, values, (err, data) => {
+            if (err) return res.status(500).json(err);
+            if (data.affectedRows>0) return res.status(200).json('User has been updated!');
+            return res.status(403).json('You can only update your profile!')
+        })
+    })
+}
